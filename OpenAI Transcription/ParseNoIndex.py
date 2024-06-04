6fcd345d40363db79ad5631c3fd1087f0c425c5a
rherbst123 <57201041+rherbst123@users.noreply.github.com>
2024-06-04 12:07:20 -0500

    Image Res Change

--- OpenAI Transcription/ParseNoIndex.py
+++ OpenAI Transcription/ParseNoIndex.py
@@ -128,7 +128,7 @@ with open(output_file, "w", encoding="utf-8") as file:
                     "type": "image_url",
                     "image_url": {
                         "url": f"data:image/jpeg;base64,{base64_image}",
-                        "detail": "High"
+                        "detail": "auto"
                     }
                 }
             ]
