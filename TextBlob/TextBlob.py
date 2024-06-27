85018e5c73901533210b7f11da7da5183cbd4d9a
rherbst123 <57201041+rherbst123@users.noreply.github.com>
2024-06-27 15:03:34 -0500

    Output to textfile.

--- TextBlob/TextBlob.py
+++ TextBlob/TextBlob.py
@@ -1,6 +1,6 @@
 from textblob import TextBlob
 
-with open("C:\\Users\\riley\\OneDrive\\Desktop\\CodeForMe\\python\\Input\\CommunityCitizenFreeResponses.txt", 'r', encoding="utf-8") as file:
+with open("Name and diretory of input.txt", 'r', encoding="utf-8") as file:
     text = file.read()
 
 lines = text.split('\n')
@@ -17,6 +17,7 @@ for line in lines:
 if current_blob:
     blobs.append(current_blob.strip())
 
-for i, blob in enumerate(blobs):
-    polarity = TextBlob(blob).sentiment.polarity
-    print(f"Line {i+1}: Polarity: {polarity}")
+with open("Name and Directory of Output.txt", 'w', encoding="utf-8") as file:
+    for i, blob in enumerate(blobs):
+        polarity = TextBlob(blob).sentiment.polarity
+        file.write(f"Line {i+1}: Polarity: {polarity}\n")
