145e04459db6900ad01f968eb6fd9cb93ff26566
dstille <dancstille@gmail.com>
2024-08-07 09:13:51 -0500

    Update accuracy.py

--- AccuracyTesting/accuracy.py
+++ AccuracyTesting/accuracy.py
@@ -12,7 +12,7 @@ GROUND_TRUTH_FILENAME = "" # First100BryophytesTyped # First25BryophytesTyped
 RESULT_FILENAME = ""
 # path wrappers are used to add folder location and add extension names
 # data can be read or saved to a diffeent folder by changing the wrapper
-SOURCE_PATH_WRAPPER = "AccuracyTesting/AccuracyTestingSources/%s.csv"
+SOURCE_PATH_WRAPPER = "Outputs/%s.csv"    # AccuracyTesting/AccuracyTestingSources/
 RESULTS_PATH_WRAPPER = "AccuracyTesting/AccuracyTestingResults/%s.csv"
 
 
@@ -33,16 +33,22 @@ def remove_skipped_fields(d: dict):
 
 # use only core fields for comparison
 def get_core_fields_only(d: dict):
-    return {key: val for key, val in d.items() if key in CORE_FIELDS_LIST}     
+    return {key: val for key, val in d.items() if key in CORE_FIELDS_LIST}
+
+# this function determines whether to use core fields or just skip select fields
+# core fields and skipped fields are set at the top
+def get_fields_to_be_compared(d:dict):
+    return remove_skipped_fields(d)   
+    # return get_core_fields_only(d)      
 
 # future home of Mr Levenshtein and his REGEX regulars
-def get_levenshtein_distance(s1, s2):
+def get_weight(s1, s2):
     return 0
 
 # returns 1 if there is a match
-# returns the value of the get_levenshtein_distance_function() if there is no match 
+# returns the value of the get_weight() function if there is no match 
 def is_match(val1, val2):
-    return val1.strip().lower() == val2.strip().lower() or get_levenshtein_distance(val1, val2)
+    return val1.strip().lower() == val2.strip().lower() or get_weight(val1, val2)
 
 # accuracy calculated only for those valid ground truth targets (i.e., not an "N/A")
 def calculate_accuracy_valid_targets(master_results_dict, num_valid_targets, num_matches):
@@ -69,20 +75,20 @@ def compare_and_tally(transcription_dicts, ground_truth_dicts, comparison_result
             elif is_match(tr_dict[fieldname], "N/A"):
                 # just in case there is not a valid target (i.e., "N/A" in the ground truth)
                    # and the llm still transcribes "N/A", the number of overall targets will still be incremented.
-                     # for this reason, the number of targets does depend a little on the llm run
+                     # for this reason, the number of targets does depend on the llm run
                 #num_targets += 1    # uncommenting this will significantly increase the number of targets
                 pass              
-    return calculate_accuracy_valid_targets(comparison_results_dict, num_targets, num_matches)                    
+    return comparison_results_dict                   
 
 def process(run_spreadname, ground_truth_dicts, blank_results_dict):
     saved_results: list[dict] = get_contents_from_csv(SOURCE_PATH_WRAPPER%run_spreadname)
-    transcription_dicts = [remove_skipped_fields(d) for d in saved_results]
-    results_dict = compare_and_tally(transcription_dicts, ground_truth_dicts, blank_results_dict)  
-    return results_dict
+    transcription_dicts = [get_fields_to_be_compared(d) for d in saved_results]
+    tallied_results_dict = compare_and_tally(transcription_dicts, ground_truth_dicts, blank_results_dict)  
+    return calculate_accuracy_valid_targets(tallied_results_dict, num_targets, num_matches) 
 
 def main(run_spreadnames=LLM_SPREAD_SOURCES): 
     reference_dicts: list[dict] = get_contents_from_csv(SOURCE_PATH_WRAPPER%GROUND_TRUTH_FILENAME)
-    ground_truth_dicts: list[dict] = [remove_skipped_fields(d) for d in reference_dicts]
+    ground_truth_dicts: list[dict] = [get_fields_to_be_compared(d) for d in reference_dicts]
     master_results = []                    
     for spreadname in run_spreadnames:
         blank_results_dict =           \
